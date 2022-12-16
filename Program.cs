e265b4416ed648ff0ac4a7b3ab2bd9461d2f1c32
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-16 15:06:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Parallel.Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
