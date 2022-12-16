58a3258de6a65b92f7838efcd46e415ba401cf0c
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-16 16:14:55 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CS.Parallel.Study
 {
@@ -6,7 +8,25 @@ namespace CS.Parallel.Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  PLINQ is effectual after IEnumerable.AsParallel()
+            //  The extension methods for PLINQ come from ParallelEnumerable static class
+            //  https://learn.microsoft.com/en-us/dotnet/api/system.linq.parallelenumerable.any?view=net-7.0
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+
+            //  PLINQ applies partition/collation strategy
+            //  Threading is handled internally by PLINQ
+            List<int> newList = queue.AsParallel().Select(n => n * 1).ToList();
         }
     }
 }
